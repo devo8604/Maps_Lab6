988e4e759ec881cc6d316aedd0a975b5412ccd77
devo8604 <devo8604@gmail.com>
2014-08-24 12:41:31 -0400

    Initial Commit

---
+++ Maps_Lab6/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Maps_Lab6
+//
+//  Created by Devon Smith on 8/24/14.
+//  Copyright (c) 2014 Devon Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
